0c498822373fbe0cab8053e96da772bad096c09a
Deyan Kalinov <Deyan Kalinov>
2016-05-15 15:50:54 +0300

    OPsWork templte

---
+++ opsworks_cookbooks/mod_php5_apache2/metadata.rb
@@ -0,0 +1,2 @@
+name        "mod_php5_apache2"
+version     "1.0.0"
--- site2/index.php
+++ site2/index.php
@@ -3,6 +3,6 @@
    <title>PHP Test</title>
     </head>
      <body>
-      <?php echo '<p>Test site</p>'; ?> 
+      <?php echo '<p>Test site 2</p>'; ?> 
      </body>
 </html>
