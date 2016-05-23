70a9611a3dcc73deb69b7aa48a94f89ae3696911
Deyan Kalinov <Deyan Kalinov>
2016-05-23 14:44:18 +0300

    Sym links

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,3 +1,5 @@
+release_path = ""
+user = "ec2-user"
 
 class Site
 	attr_accessor :name, :directory, :link
@@ -14,11 +16,9 @@ site2.directory = "images"
 site2.link = "/mnt/nfs/site2"
 
 sites = [site1, site2]
-
-ruby_block "Sym link creation" do
-	for site in sites	
+for site in sites	
 		Chef::Log.info("Symlink cd #{release_path} && ln -s #{site.link} ./#{site.name}/#{site.directory}")
-		Chef::Log.info(`cd #{release_path} && ln -s #{site.link} ./#{site.name}/#{site.directory}`)
+		Chef::Log.info(`sudo -H -u #{user} bash -c 'cd #{release_path} && ln -s #{site.link} ./#{site.name}/#{site.directory}'`)
 		raise "Symlink failed, server wonn't come up" unless $?.success?
-	end
 end
+
