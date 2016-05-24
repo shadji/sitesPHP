f5f8c3f48b14203da273d57b50b781d9e2af7615
Deyan Kalinov <Deyan Kalinov>
2016-05-24 15:20:23 +0300

    .

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -25,7 +25,7 @@ for site in sites
 	Chef::Log.info(`sudo chown #{user}:#{group} #{site.link}`)
 	Chef::Log.info(`sudo chmod #{mode} #{site.link}`)
 	Chef::Log.info(`sudo chmod g+s #{site.link}`)
-	Chef::Log.info("Createing symlink: cd #{release_path} && ln -s #{site.link} ./#{site.name}/#{site.directory}")
+	Chef::Log.info("Createing symlink: sudo -H -u #{user} bash -c 'cd #{release_path} && ln -s #{site.link} ./#{site.name}/#{site.directory}")
 	Chef::Log.info(`sudo -H -u #{user} bash -c 'cd #{release_path} && ln -s #{site.link} ./#{site.name}/#{site.directory}'`)
 	raise "Symlink failed, server wonn't come up" unless $?.success?
 end
