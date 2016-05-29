7489024ac502ce5f3da4714b181e80240e91ff90
Deyan Kalinov <Deyan Kalinov>
2016-05-29 13:44:47 +0300

    Links created in a loop

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -5,14 +5,14 @@ mode = "0770"
 basepath = node[:nfs_client][:nfs_mount_point]
  
 node[:sites].each do |site|
-	site[:directories].each do |directory, link|
+	site[:directories].each do |directory|
 		Chef::Log.info("Createing directory, if don't exisits")
-		Chef::Log.info(`sudo mkdir -p #{basepath}/#{link}`)
-		Chef::Log.info(`sudo chown #{user}:#{group} #{basepath}/#{link}`)
-		Chef::Log.info(`sudo chmod #{mode} #{basepath}/#{link}`)
-		Chef::Log.info(`sudo chmod g+s #{basepath}/#{link}`)
-		Chef::Log.info("Createing symlink: sudo -H -u #{user} bash -c 'cd #{release_path} && ln -s #{basepath}/#{link} ./#{site.name}/#{directory}")
-		Chef::Log.info(`sudo -H -u #{user} bash -c 'cd #{release_path} && ln -s #{basepath}/#{link} ./#{site.name}/#{directory}'`)
+		Chef::Log.info(`sudo mkdir -p #{basepath}/#{directory[:link]}`)
+		Chef::Log.info(`sudo chown #{user}:#{group} #{basepath}/#{directory[:link]}`)
+		Chef::Log.info(`sudo chmod #{mode} #{basepath}/#{directory[:link]}`)
+		Chef::Log.info(`sudo chmod g+s #{basepath}/#{directory[:link]}`)
+		Chef::Log.info("Createing symlink: sudo -H -u #{user} bash -c 'cd #{release_path} && ln -s #{basepath}/#{directory[:link]} ./#{site.name}/#{directory[:directory]}")
+		Chef::Log.info(`sudo -H -u #{user} bash -c 'cd #{release_path} && ln -s #{basepath}/#{directory[:link]} ./#{site.name}/#{directory[:directory]}'`)
 		raise "Symlink failed, server wonn't come up" unless $?.success?
 	end
 end
