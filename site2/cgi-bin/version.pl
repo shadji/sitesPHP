0bf5a4224ffa90d06de2f9e5a634fba39f2b70fd
Slav Hadjidimitrov <shadji@users.noreply.github.com>
2016-06-04 18:10:15 +0300

    Update version.pl

--- site2/cgi-bin/version.pl
+++ site2/cgi-bin/version.pl
@@ -1,11 +1,19 @@
 #!/usr/bin/perl
 # Available under BSD License.
 # See url http://www.cyberciti.biz/faq/how-can-i-find-out-perl-version/
+
+use POSIX;
+use CGI;
+
+$cgi = CGI->new;
+$id = $cgi->param('id');
+
+print $cgi->header();
+
 $command=`perl -v`;
 $title = "Perl Version";
  
-print "Content-type: text/html\n\n";
-print "<html><head><title>$title</title></head><body>";
+
  
 print "<h1>$title</h1>\n";
  
